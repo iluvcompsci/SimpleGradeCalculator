e0d30f08cecb09bacd201fb00e594df4fdb5b8b4
iluvcompsci <bri.chapman@gmail.com>
2014-09-02 01:32:51 -0500

    initial project

---
+++ GradeTracker/GradeTracker/BCCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BCCAppDelegate.h
+//  GradeTracker
+//
+//  Created by Briana Chapman on 9/2/14.
+//  Copyright (c) 2014 Briana Chapman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GradeTracker/GradeTracker/BCCViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCCViewController.h
+//  GradeTracker
+//
+//  Created by Briana Chapman on 9/2/14.
+//  Copyright (c) 2014 Briana Chapman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCCViewController : UIViewController
+
+@end
